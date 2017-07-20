90088e89e15ce47caa39e189e1020e0ab3276397
Hemken <hemken@ADS.SSC.WISC.EDU>
2017-07-20 11:06:48 -0500

    clean up initial

--- R/stdBeta.r
+++ R/stdBeta.r
@@ -1,9 +1,9 @@
 stdBeta <- function(lmfit) {
   stopifnot(class(lmfit)=="lm")
-  stddata <- model.frame(lmfit)
+  stddata <- stats::model.frame(lmfit)
   # print(str(df))
   numvars <- sapply(stddata, is.numeric)
   stddata[, numvars] <- sapply(stddata[numvars], scale)
   # head(df)
-  update(lmfit, data=stddata)
+  stats::update(lmfit, data=stddata)
 }
--- inst/doc/example.r
+++ inst/doc/example.r
@@ -1,16 +1,8 @@
+library(stdBeta)
+
 fit <- lm(mpg ~ disp*wt, mtcars)
 mtcars$Cyl <- as.factor(mtcars$cyl)
 fit2 <- lm(mpg ~ disp*Cyl, mtcars)
-
-stdBeta <- function(lmfit) {
-  stopifnot(class(lmfit)=="lm")
-  stddata <- model.frame(lmfit)
-  # print(str(df))
-  numvars <- sapply(stddata, is.numeric)
-  stddata[, numvars] <- sapply(stddata[numvars], scale)
-  # head(df)
-  update(lmfit, data=stddata)
-}
 
 fit
 stdBeta(fit)
